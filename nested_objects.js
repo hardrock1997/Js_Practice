ed00c200b072309246bf157fb7560d36b0c3c4b6
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2025-07-03 16:05:49 +0530

    Update nested_objects.js

--- nested_objects.js
+++ nested_objects.js
@@ -60,6 +60,57 @@ function flatten(input) {
         }
     }
     return toReturn
+
+
+
+const obj1 = {
+  a: 1,
+  b: { x: 10, y: 20 },
+  d: { z: 100 }
+};
+const obj2 = {
+  b: { y: 30, k: 40 },
+  c: 3,
+  d: 4
+};
+
+/*
+✅ Expected Output:
+{
+  a: 1,
+  b: { x: 10, y: 30, k: 40 },
+  c: 3,
+  d: 4
+}
+*/
+
+const res = helper(obj1, obj2)
+
+console.log(res)
+
+function helper(obj1, obj2) {
+    if((!obj1 && obj2) || (obj1 && !obj2)) {
+        return "invalid inputs"
+    }
+    let toReturn={}
+    const keys1=Object.keys(obj1)
+    const keys2=Object.keys(obj2)
+    for(const key of keys1) {
+        toReturn[key]=obj1[key]
+    }
+    
+    for(const key of keys2) {
+        if(typeof obj2[key]==="object") {
+            toReturn[key]={...toReturn[key],...obj2[key]}
+        }
+        else toReturn[key]=obj2[key]
+        
+    }
+    
+    return toReturn
+    
+    
+}
     
 }
 
